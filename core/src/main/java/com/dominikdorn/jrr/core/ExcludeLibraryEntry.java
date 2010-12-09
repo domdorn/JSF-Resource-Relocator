b00795592f0802f20cfd19a9e007d0f3dca46bdc
Dominik Dorn <dominik@dominikdorn.com>
2010-12-09 19:19:46 +0100

    added ExcludeLibraryEntry including Documentation

--- core/src/main/java/com/dominikdorn/jrr/core/ExcludeLibraryEntry.java
+++ core/src/main/java/com/dominikdorn/jrr/core/ExcludeLibraryEntry.java
@@ -1,10 +1,14 @@
 package com.dominikdorn.jrr.core;
 
 /**
- * Dominik Dorn
- * 0626165
- * dominik.dorn@tuwien.ac.at
+ * This <em>LibraryEntry</em> says we want to <em>Exclude</em>
+ * this <em>Resource</em> from the <em>Library</em>.
+ *
+ * What this means is, that a request for a excluded <em>Resource</em>
+ * should result in a <em>null</em> Value returned by the <em>resolver</em>
+ * which then leads to ignoring the <em>Resource</em> while rendering.
+ *
  */
 public class ExcludeLibraryEntry extends LibraryEntry {
-    
+/* name already given in abstract LibraryEntry */
 }
