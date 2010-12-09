f8d28b0cf2db98e80e0b6a1e107846f7e5232beb
Dominik Dorn <dominik@dominikdorn.com>
2010-12-09 19:20:36 +0100

    added ReplaceLibraryEntry including Documentation

--- core/src/main/java/com/dominikdorn/jrr/core/ReplaceLibraryEntry.java
+++ core/src/main/java/com/dominikdorn/jrr/core/ReplaceLibraryEntry.java
@@ -1,9 +1,23 @@
 package com.dominikdorn.jrr.core;
 
 /**
- * Dominik Dorn
- * 0626165
- * dominik.dorn@tuwien.ac.at
+ * This <em>LibraryEntry</em> is supposed to replace an existing
+ * <em>LibraryEntry</em> (not necessarily specified in the configuration file)
+ * with an reference to another <em>LibraryEntry</em>.
+ *
+ * This effectively allows to replace a certain script (e.g. <em>jquery.js</em>)
+ * in one <em>Library</em> (e.g. <em>primefaces</em>) with a <em>resource</em>
+ * within another <em>Library</em> (pointing to an <em>id</em> of the other <em>LibraryEntry</em>).
  */
 public class ReplaceLibraryEntry extends LibraryEntry {
+
+    String with;
+
+    public String getWith() {
+        return with;
+    }
+
+    public void setWith(String with) {
+        this.with = with;
+    }
 }
