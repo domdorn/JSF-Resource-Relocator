59dddc0a41fd85b03735a80f8a6bdfe8b9e94f15
Dominik Dorn <dominik@dominikdorn.com>
2010-12-10 19:38:19 +0100

    Primitive ResourceResolver

---
+++ core/src/main/java/com/dominikdorn/jrr/jsf/PrimitiveResourceResolver.java
@@ -0,0 +1,150 @@
+package com.dominikdorn.jrr.jsf;
+
+import com.dominikdorn.jrr.core.domain.*;
+
+/**
+ * A primitive ResourceResolver without using hashmaps etc.<br/>
+ * <strong>Just a prove of concept.</strong>
+ */
+public class PrimitiveResourceResolver {
+
+    Relocator relocator;
+
+    public PrimitiveResourceResolver(Relocator relocator) {
+        this.relocator = relocator;
+    }
+
+
+    public String getResourceURL(String libraryId, String resourceName) {
+        if (libraryId == null || resourceName == null || libraryId.isEmpty() || resourceName.isEmpty())
+            throw new NullPointerException();
+
+//        Library l = getLibrary(relocator, libraryId);
+//        if (l == null)
+//            return null;
+
+        LibraryEntry le = getLibraryEntry(relocator, libraryId, resourceName);
+
+//        for (LibraryEntry le : l.getEntries()) {
+//            if (le.getName().equals(resourceName)) {
+//                // found LE
+//
+//            }
+//        }
+        Library l;
+        if(le == null)
+        {
+            l = getLibrary(relocator, libraryId);
+            if(l == null)
+                return null;
+        }
+        else
+        {
+
+            if(le instanceof ResourceLibraryEntry)
+                le = resolveFinalLibraryEntry(relocator, (ResourceLibraryEntry) le);
+            if(le instanceof ExcludeLibraryEntry)
+                le = resolveFinalLibraryEntry(relocator, (ExcludeLibraryEntry) le);
+            if(le instanceof ReplaceLibraryEntry){
+                le = resolveFinalLibraryEntry(relocator, (ReplaceLibraryEntry) le);
+                if(le == null)
+                    return null;
+                if(le instanceof ResourceLibraryEntry)
+                    if( ((ResourceLibraryEntry) le).getAbsoluteUrl() != null)
+                        return ((ResourceLibraryEntry) le).getAbsoluteUrl();
+
+            }
+
+            if(le == null)
+                return null;
+
+            l = le.getParent();
+
+            resourceName = le.getName();
+        }
+
+        Mirror m = getMirrorForLibrary(relocator, l.getId());
+        if (m == null)
+            return null;
+
+        return m.getBasePath() + "/" +l.getId() +"/" + resourceName;
+
+
+
+    }
+
+    private LibraryEntry getLibraryEntry(Relocator relocator, String libraryId, String resourceName) {
+        Library l = getLibrary(relocator, libraryId);
+        if (l != null) {
+            for (LibraryEntry le : l.getEntries()) {
+                if (le.getName().equals(resourceName)) {
+                    // found LE
+                    return le;
+                }
+            }
+        }
+        return null;
+    }
+
+    private Library getLibrary(Relocator relocator, String libraryId) {
+        for (Library l : relocator.getLibraries()) {
+            if (l.getId().equals(libraryId)) {
+                return l;
+            }
+        }
+        return null;
+
+    }
+
+    private Mirror getMirrorForLibrary(Relocator relocator, String libraryId) {
+        for (Mirror m : relocator.getMirrors()) {
+            for (String s : m.getLibraries()) {
+                if (s.equals(libraryId)) {
+                    // this mirror is a mirror of this library
+                    return m;
+                }
+            }
+        }
+        return null;
+    }
+
+    private LibraryEntry resolveFinalLibraryEntry(Relocator relocator, ResourceLibraryEntry le)
+    {
+        return le;
+    }
+
+    private LibraryEntry resolveFinalLibraryEntry(Relocator relocator, ReplaceLibraryEntry le)
+    {
+        LibraryEntry replacement = getLibraryEntryWithId(relocator, le.getWith());
+        if(replacement == null)
+            return null;
+        return replacement;
+    }
+
+    private LibraryEntry resolveFinalLibraryEntry(Relocator relocator, ExcludeLibraryEntry le)
+    {
+        return null;
+    }
+
+    private LibraryEntry resolveFinalLibraryEntry(Relocator relocator, LibraryEntry le)
+    {
+        throw new IllegalStateException("may never happen");
+    }
+
+
+    private LibraryEntry getLibraryEntryWithId(Relocator relocator, String id)
+    {
+        if(id == null || id.isEmpty())
+            return null;
+
+        for(Library l : relocator.getLibraries())
+        {
+            for(LibraryEntry le : l.getEntries())
+            {
+                if(le.getId() != null && le.getId().equals(id))
+                    return le;
+            }
+        }
+        return null;
+    }
+}
---
+++ core/src/test/java/com/dominikdorn/jrr/jsf/ResourceResolverTest.java
@@ -0,0 +1,152 @@
+package com.dominikdorn.jrr.jsf;
+
+import com.dominikdorn.jrr.core.domain.*;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Dominik Dorn
+ * 0626165
+ * dominik.dorn@tuwien.ac.at
+ */
+public class ResourceResolverTest {
+
+    private final String GOOGLE_JQUERY_14 = "http://cdn.google.com/jquery/jquery_1.4-min.js";
+
+    Relocator relocator1;
+
+    PrimitiveResourceResolver resolver1;
+
+    Library primeFaces;
+    Library prettyFaces;
+
+    Library googleJQuery;
+    Library dominikJQuery;
+
+    ResourceLibraryEntry google_jquery_14;
+    ReplaceLibraryEntry primefaces_jquery;
+    ResourceLibraryEntry dominik_jquery_15;
+
+
+    Mirror hetzner;
+    Mirror aws_s3;
+
+    @Before
+    public void setUp() throws Exception {
+        relocator1 = new Relocator();
+        /* libraries */
+        primeFaces = new Library();
+        primeFaces.setId("primefaces");
+
+        prettyFaces = new Library();
+        prettyFaces.setId("prettyfaces");
+
+        googleJQuery = new Library();
+        googleJQuery.setId("google_jquery");
+
+        dominikJQuery = new Library();
+        dominikJQuery.setId("dominik_jquery");
+
+
+
+        /* resources */
+        google_jquery_14 = new ResourceLibraryEntry();
+        google_jquery_14.setId("google_jquery_14");
+        google_jquery_14.setName("jquery/jquery_1.4-min.js");
+        google_jquery_14.setAbsoluteUrl(GOOGLE_JQUERY_14);
+
+        primefaces_jquery = new ReplaceLibraryEntry();
+        primefaces_jquery.setName("jquery/jquery.js");
+        primefaces_jquery.setWith("google_jquery_14");
+
+
+
+        dominik_jquery_15 = new ResourceLibraryEntry();
+        dominik_jquery_15.setId("dominik_jquery_15");
+        dominik_jquery_15.setName("jquery/jquery_15.js");
+
+
+        /* mapping resources to libraries */
+
+        googleJQuery.addEntry(google_jquery_14);
+        primeFaces.addEntry(primefaces_jquery);
+        dominikJQuery.addEntry(dominik_jquery_15);
+
+        relocator1.addLibrary(googleJQuery);
+        relocator1.addLibrary(primeFaces);
+        relocator1.addLibrary(dominikJQuery);
+
+        /* Mirrors */
+        hetzner = new Mirror();
+        hetzner.setId("hetzner");
+        hetzner.setBasePath("http://static.studyguru.net/resources/");
+
+        aws_s3 = new Mirror();
+        aws_s3.setId("aws_s3");
+        aws_s3.setBasePath("http://aws.studyguru.net/resources/");
+
+        relocator1.addMirror(hetzner);
+        relocator1.addMirror(aws_s3);
+
+        /* creating the ResourceResolver */
+        resolver1 = new PrimitiveResourceResolver(relocator1);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void getResourceURL_null_NPE_expected()
+    {
+        resolver1.getResourceURL(null, null);
+        fail();
+    }
+
+    @Test
+    public void getResourceURL_notManagedLibrary()
+    {
+        assertNull(resolver1.getResourceURL("unknowLibrary", "jquery/jquery.js"));
+    }
+
+    @Test
+    public void getResourceURL_managedLibrary_unknownResource_mirrored()
+    {
+        hetzner.addLibrary("primefaces");
+
+        String url = resolver1.getResourceURL("primefaces", "jquery/lightbox.js");
+        assertNotNull(url);
+        Assert.assertEquals(hetzner.getBasePath()+"/primefaces/"+"jquery/lightbox.js", url);
+    }
+
+    @Test
+    public void getResourceURL_managedLibrary_unknownResource_notMirrored()
+    {
+        String url = resolver1.getResourceURL("primefaces", "jquery/lightbox.js");
+        assertNull(url);
+    }
+
+    @Test
+    public void getResourceURL_managedLibrary_replaceResource()
+    {
+
+        String url = resolver1.getResourceURL("primefaces", "jquery/jquery.js");
+        assertNotNull(url);
+        assertEquals(GOOGLE_JQUERY_14, url);
+    }
+
+    @Test
+    public void getResourceURL_managedLibrary_replaceResource_otherManagedResource()
+    {
+        hetzner.addLibrary("dominik_jquery");
+        primefaces_jquery.setWith("dominik_jquery_15");
+        String url = resolver1.getResourceURL("primefaces", "jquery/jquery.js");
+        assertNotNull(url);
+        assertEquals(hetzner.getBasePath()+"/"+dominikJQuery.getId()+"/"+dominik_jquery_15.getName(), url);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
