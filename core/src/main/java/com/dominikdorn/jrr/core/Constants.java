78acf290503cee03f673714d0de2f7ff98162963
Dominik Dorn <dominik@dominikdorn.com>
2010-12-11 16:50:11 +0100

    added constant for ResourceResolver as well.

--- core/src/main/java/com/dominikdorn/jrr/core/Constants.java
+++ core/src/main/java/com/dominikdorn/jrr/core/Constants.java
@@ -8,5 +8,6 @@ package com.dominikdorn.jrr.core;
 public final class Constants {
 
     public final static String CONFIGURATION_FILE = "/WEB-INF/jsf-resource-relocator.xml";
-    public final static String CONTEXT_ATTRIBUTE_NAME = "jrr_relocator";
+    public final static String CONTEXT_RELOCATOR_ATTRIBUTE_NAME = "jrr_relocator";
+    public final static String CONTEXT_RESOURCE_RESOLVER_ATTRIBUTE_NAME = "jrr_resource_resolver";
 }
